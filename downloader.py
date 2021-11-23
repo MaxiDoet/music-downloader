040accdeee8a71786122d14deaa7a60a4b0b0e4e
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-11-23 17:03:49 +0100

    First version

---
+++ downloader.py
@@ -0,0 +1,42 @@
+import colorama
+from ffmpeg.nodes import output_operator
+from pytube import YouTube, Playlist
+import ffmpeg
+import sys, os, pathlib, re
+
+def download_mp3(video : YouTube, index=1, playlist_length=1):
+    stream = video.streams.get_audio_only()
+    output_file = stream.download()
+
+    print("[%sdownload%s] %d/%d %s%s%s" % (colorama.Fore.BLUE, colorama.Fore.RESET, index, playlist_length, colorama.Fore.GREEN, video.title, colorama.Fore.RESET))
+
+    #os.rename(output_file, re.sub(r'[\\/*?:"<>|]',"", video.title + ".mp3"))
+    new_filename = re.sub(r'[\\/*?:"<>|]',"", video.title + ".mp3")
+    converted_stream = ffmpeg.input(output_file).output(new_filename).run()
+    os.remove(output_file)
+
+def download_playlist(playlist_url):
+    playlist = Playlist(playlist_url)
+
+    print("[%splaylist%s] Title: %s%s%s" % (colorama.Fore.BLUE, colorama.Fore.RESET, colorama.Fore.GREEN, playlist.title, colorama.Fore.RESET))
+    print("[%splaylist%s] Length: %s%d%s" % (colorama.Fore.BLUE, colorama.Fore.RESET, colorama.Fore.GREEN, len(playlist), colorama.Fore.RESET))
+
+    index = 1
+    for video in playlist.videos:
+        download_mp3(video, index, playlist.length)
+        index+=1
+
+    print("[%splaylist%s] %sDone%s" % (colorama.Fore.BLUE, colorama.Fore.RESET, colorama.Fore.GREEN, colorama.Fore.RESET))
+
+# Init color console
+colorama.init()
+try:
+    input_url = sys.argv[1]
+except:
+    print("%sNo url provided!%s" % (colorama.Fore.RED, colorama.Fore.RESET))
+    exit(-1)
+
+if input_url.find("playlist") == 0:
+    download_playlist(input_url)
+else:
+    download_mp3(YouTube(input_url))
