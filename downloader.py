5b7aebf8547289e5c1d79e753b23bd55e7ff03e4
MaxiDoet <maximilian.doetsch@outlook.de>
2024-12-01 11:30:27 +0100

    Switched to yt-dlp

--- downloader.py
+++ downloader.py
@@ -4,7 +4,7 @@ import colorama
 from ffmpeg.nodes import output_operator
 from numpy import source
 from pip import List
-from pytube import YouTube, Playlist, Search
+from yt_dlp import YoutubeDL
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import ffmpeg
@@ -42,43 +42,31 @@ args = parser.parse_args()
 
 SOURCE_TYPES = ["YouTube", "Spotify"]
 
-def download_youtube_video(video : YouTube, folder, index=1, playlist_length=1):
-    filename = re.sub(r'[\\/*?:"<>|]',"", video.title + ".mp3")
+def download_youtube_video(url, folder, index=1, playlist_length=1):
+    yt = YoutubeDL({'quiet': True, 'paths': {'home': 'files/' + folder}, 'format': 'm3a/bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3',}]})
+    info = yt.extract_info(url, download=False)
+
+    filename = re.sub(r'[\\/*?:"<>|]',"", info["title"] + ".mp3")
     filepath = "files/" + folder + "/" + filename
 
     spinner = Halo(text='%d/%d Fetching' % (index, playlist_length), spinner='dots')
 
     if not os.path.exists("files/" + folder):
-        os.mkdir("files/" + folder)
+        os.makedirs("files/" + folder)
 
     if os.path.exists(filepath):
         if not args.r:
-            spinner.warn("%d/%d %s" % (index, playlist_length, video.title))
+            spinner.warn("%d/%d %s" % (index, playlist_length, info["title"]))
             return
         else:
             os.remove(filepath)
 
-    spinner.start()
-    stream = video.streams.get_audio_only()
-    spinner.stop()
-
     spinner.text = "%d/%d Downloading" % (index, playlist_length)
     spinner.start()
-    output_file = stream.download()
+    yt.download(url)
     spinner.stop()
 
-    spinner.text = "%d/%d Converting" % (index, playlist_length)
-    spinner.start()
-    try:
-        converted_stream = ffmpeg.input(output_file).output(filepath).run(quiet=True)
-    except ffmpeg._run.Error:
-        spinner.fail("%d/%d FFmpeg error" % (index, playlist_length))
-        return
-
-    os.remove(output_file)
-
-    spinner.stop()
-    spinner.succeed("%d/%d %s" % (index, playlist_length, video.title))
+    spinner.succeed("%d/%d %s" % (index, playlist_length, info["title"]))
 
 class SourceInfo:
     def __init__(self, single, type, title, owner, trackCount, tracks):
@@ -107,15 +95,7 @@ class SourceInfo:
         elif self.type == 1:
             for i in range(len(self.tracks)):
                 download_youtube_video(self.tracks[i], self.title, i+1, self.trackCount)
-
-def fetch_youtube_video(url) -> SourceInfo:
-    video = YouTube(url)
-    return SourceInfo(True, 0, video.title, video.author, 1, [YouTube(url)])
-
-def fetch_youtube_playlist(url) -> SourceInfo:
-    playlist = Playlist(url)
-    return SourceInfo(False, 0, playlist.title, "Unknown", len(playlist.videos), playlist.videos)
-
+    
 def find_youtube_video(query):
     """
     results = Search(title).results
@@ -131,11 +111,11 @@ def find_youtube_video(query):
 
         if category == "Songs" and result_type == "song":
             result_id = results[i]["videoId"]
-            return YouTube("https://youtube.com/watch?v=%s" % (result_id))
+            return "https://youtube.com/watch?v=%s" % (result_id)
         else:
             pass
 
-    return YouTube("https://youtube.com/watch?v=%s" % (result_id))
+    return "https://youtube.com/watch?v=%s" % (result_id)
 
 def fetch_spotify_playlist(url) -> SourceInfo:
     playlist = sp.playlist(str(url))
@@ -173,15 +153,7 @@ spinner = Halo(text='Please wait', spinner='dots')
 spinner.start()
 
 # Fetch playlist
-if "youtube.com" in input_url:
-    spinner.stop()
-
-    if "youtube.com/playlist?list" in input_url:
-        source_info = fetch_youtube_playlist(input_url)
-    else:
-        source_info = fetch_youtube_video(input_url)
-        
-elif "spotify.com" in input_url:
+if "spotify.com" in input_url:
     spinner.stop()
     source_info = fetch_spotify_playlist(input_url)
 else:
